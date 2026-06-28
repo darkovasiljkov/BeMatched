4e4d717b9d4d2b211e0254b0dfcfd9c94e658772
darkovasiljkov <vasiljkovdarko@gmail.com>
2026-06-29 01:08:26 +0200

    add: member-details routes & created empty components for details page

--- Client/src/features/members/member-detailed/member-detailed.ts
+++ Client/src/features/members/member-detailed/member-detailed.ts
@@ -1,13 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
-import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
@@ -16,9 +17,20 @@ export class MemberDetailed implements OnInit {
   private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   protected member$?: Observable<Member>;
+  private router = inject(Router);
+  protected title = signal<string | undefined>('Profile');
 
     ngOnInit(): void {
     this.member$ = this.loadMember();
+    this.title.set(this.route.firstChild?.snapshot?.title);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe({
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      }
+    })
   }
 
   loadMember() {
